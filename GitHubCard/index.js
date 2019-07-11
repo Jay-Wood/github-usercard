da62b22919ab3598500264a69a5a38a60022919f
Jay Wood <47437698+jaywood8@users.noreply.github.com>
2019-07-11 14:22:47 -0700

    receiving data from github API

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -3,6 +3,11 @@
            https://api.github.com/users/<your name>
 */
 
+const promise = axios.get(`https://api.github.com/users/Jay-Wood`)
+  .then(data => {
+    console.log("worked",data)
+  })
+
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
    data in order to use it to build your component function 
